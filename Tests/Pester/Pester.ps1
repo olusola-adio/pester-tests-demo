f7fd5d88fa1e346cc12183ca35c5aaee533339db
olusola-adio <olusola_adio@hotmail.com>
2021-09-29 20:51:20 +0100

    pester

--- Tests/Pester/Pester.ps1
+++ Tests/Pester/Pester.ps1
@@ -61,7 +61,7 @@ if (!$pesterModule) {
 Write-Host "Pester version: $($pesterModule.Version.Major).$($pesterModule.Version.Minor).$($pesterModule.Version.Build)"
 $pesterModule | Import-Module
 
-Import-Module ..\..\Resources\PowerShellScripts\DemoModule.psm1 -Force
+Import-Module .`Resources\PowerShellScripts\DemoModule.psm1 -Force
 
 if ($Publish) {
     if (!(Test-Path -Path $ResultsPath)) {
