27ddfb5b402dcd9e4b9026b533fc38f066aa86d7
olusola-adio <olusola_adio@hotmail.com>
2021-10-01 17:02:56 +0100

    simulated inputs for the acceptance tests

--- Tests/Pester07.AddNumbers.Acceptance.Tests.ps1
+++ Tests/Pester07.AddNumbers.Acceptance.Tests.ps1
@@ -17,41 +17,58 @@ BeforeDiscovery {
 
 Describe "Acceptance Tests" -Tag "Acceptance" {
 
+    BeforeDiscovery {
+        $numbersOneParam = @()
+        for ($i = 1; $i -le 50; $i++) {
+            $input1 = $i + $i + 1
+            $answer = $($input1 + 2)
+            $numbersOneParam += @{number1 = $input1; number2 = $input2; expected = $answer }
+        }
+        $numbersBothParams = @()
+        for ($i = 1; $i -le 50; $i++) {
+            $input1 = $i + $i + 1
+            $input2 = $i + 37 - 2
+            $answer = $($input1 + $input2)
+            $numbersBothParams += @{number1 = $input1; number2 = $input2; expected = $answer }
+        }
+    }
+    BeforeAll {
+        $numbersOneParam = @()
+        for ($i = 1; $i -le 50; $i++) {
+            $input1 = $i + $i + 1
+            $answer = $($input1 + 2)
+            $numbersOneParam += @{number1 = $input1; number2 = $input2; expected = $answer }
+        }
+        $numbersBothParams = @()
+        for ($i = 1; $i -le 50; $i++) {
+            $input1 = $i + $i + 1
+            $input2 = $i + 37 - 2
+            $answer = $($input1 + $input2)
+            $numbersBothParams += @{number1 = $input1; number2 = $input2; expected = $answer }
+        }
+    }
+
     Context "when both parameters are used" {
         It "<number1> plus <number2> Returns <expected>" -ForEach @(
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
-            @{ number1 = 1; number2 = 2; expected = 3 }
             @{ number1 = 1; number2 = 2; expected = 3 }
         ) {
             AddNumbers -number1 $number1 -number2 $number2 | Should -Be $expected
         }
+
+        It "<number1> plus <number2> Returns <expected>" -ForEach @($numbersBothParams) {
+            AddNumbers -number1 $number1 -number2 $number2 | Should -Be $expected
+        }
+
     }
 
     Context "when one parameter is used" {
         It "number1 = <number1> Returns <expected>" -ForEach @(
             @{ number1 = 1; expected = 3 }
         ) {
+            AddNumbers -number1 $number1 | Should -Be $expected
+        }
+
+        It "number1 = <number1> Returns <expected>" -ForEach @($numbersOneParam) {
             AddNumbers -number1 $number1 | Should -Be $expected
         }
     }
--- Tests/Pester08.SubtractNumbers.Acceptance.Tests.ps1
+++ Tests/Pester08.SubtractNumbers.Acceptance.Tests.ps1
@@ -17,30 +17,58 @@ BeforeDiscovery {
 
 Describe "Acceptance Tests" -Tag "Acceptance" {
 
+    BeforeDiscovery {
+        $numbersOneParam = @()
+        for ($i = 1; $i -le 50; $i++) {
+            $input1 = $i + $i + 1
+            $answer = $($input1 - 0)
+            $numbersOneParam += @{number1 = $input1; number2 = $input2; expected = $answer }
+        }
+        $numbersBothParams = @()
+        for ($i = 1; $i -le 50; $i++) {
+            $input1 = $i + 37 - 2
+            $input2 = $i + $i + 1
+            $answer = $($input1 - $input2)
+            $numbersBothParams += @{number1 = $input1; number2 = $input2; expected = $answer }
+        }
+    }
+    BeforeAll {
+        $numbersOneParam = @()
+        for ($i = 1; $i -le 50; $i++) {
+            $input1 = $i + $i + 1
+            $answer = $($input1 - 0)
+            $numbersOneParam += @{number1 = $input1; number2 = $input2; expected = $answer }
+        }
+        $numbersBothParams = @()
+        for ($i = 1; $i -le 50; $i++) {
+            $input1 = $i + 37 - 2
+            $input2 = $i + $i + 1
+            $answer = $($input1 - $input2)
+            $numbersBothParams += @{number1 = $input1; number2 = $input2; expected = $answer }
+        }
+    }
+
     Context "when both parameters are used" {
         It "<number1> minus <number2> Returns <expected>" -ForEach @(
-            @{ number1 = 1; number2 = 2; expected = -1 }
-            @{ number1 = 31; number2 = 2; expected = 29 }
-            @{ number1 = 1; number2 = 2; expected = -1 }
-            @{ number1 = 31; number2 = 2; expected = 29 }
-            @{ number1 = 1; number2 = 2; expected = -1 }
-            @{ number1 = 31; number2 = 2; expected = 29 }
-            @{ number1 = 1; number2 = 2; expected = -1 }
-            @{ number1 = 31; number2 = 2; expected = 29 }
-            @{ number1 = 1; number2 = 2; expected = -1 }
-            @{ number1 = 31; number2 = 2; expected = 29 }
+            @{ number1 = 81; number2 = 2; expected = 79 }
         ) {
             SubtractNumbers -number1 $number1 -number2 $number2 | Should -Be $expected
         }
+
+        It "<number1> minus <number2> Returns <expected>" -ForEach @($numbersBothParams) {
+            SubtractNumbers -number1 $number1 -number2 $number2 | Should -Be $expected
+        }
+
     }
 
     Context "when one parameter is used" {
         It "number1 = <number1> Returns <expected>" -ForEach @(
-            @{ number1 = 6; expected = 6 }
-            @{ number1 = 6; expected = 6 }
-            @{ number1 = 6; expected = 6 }
-            @{ number1 = 6; expected = 6 }
+            @{ number1 = 5; expected = 5 }
         ) {
+            SubtractNumbers -number1 $number1 | Should -Be $expected
+        }
+
+        It "number1 = <number1> Returns <expected>" -ForEach @($numbersOneParam) {
             SubtractNumbers -number1 $number1 | Should -Be $expected
         }
     }
